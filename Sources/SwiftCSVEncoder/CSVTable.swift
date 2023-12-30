d11c28b4c2cd3124d195ff96740c4d0290badcb8
Scott Matthewman <scott@matthewman.net>
2023-12-30 15:52:43 +0000

    Document `CSVTable`

--- Sources/SwiftCSVEncoder/CSVTable.swift
+++ Sources/SwiftCSVEncoder/CSVTable.swift
@@ -7,10 +7,66 @@
 
 import Foundation
 
-public struct CSVTable<Record> {
-    private var columns: [CSVColumn<Record>]
-    private var configuration: CSVEncoderConfiguration
+/**
+ The definition of a CSV file structure.
 
+ CSV tables are keyed to a specific record type, with each row of the CSV file presumed to map a
+ single item of that type. The ``columns`` property describes the columns of the CSV file, left to right,
+ and how the values are derived from each row's source record.
+
+ An additional ``configuration`` property encapsulates decisions that may affect how the overall
+ CSV file is formatted.
+
+ ```swift
+ struct Person {
+   var firstName: String
+   var lastName: String
+   var occupation: String?
+   var employer: Business?
+   var tags: [String]
+ }
+
+ struct Business {
+   var name: String
+   var description: String?
+ }
+
+ let peopleTable = CSVTable<Person>(
+   columns: [
+     CSVColumn("First name") { $0.firstName },
+     CSVColumn("Last name") { $0.lastName },
+     CSVColumn("Occupation") { $0.occupation },
+     CSVColumn("Employer") { $0.business?.name },
+     CSVColumn("Tags") { $0.tags.joined(separator: ", ") }
+   ],
+   configuration: .default
+ )
+
+
+ let people: [Person] = [
+   /* ..definition of `Person` objects */
+ ]
+
+ let csvTable = peopleTable.export(rows: people)
+
+ ```
+
+ Note that ``CSVTable/export(rows:)`` only serializes the supplied data into a `String` object. It is up to the calling application to persist that text as a file.
+
+*/
+public struct CSVTable<Record> {
+    /// A description of all the columns of the CSV file, order from left to right.
+    public private(set) var columns: [CSVColumn<Record>]
+    /// The set of configuration parameters to use while encoding attributes and the whole file.
+    ///
+    /// The default configuration is ``CSVEncoderConfiguration/default``, which provides a date
+    /// encoding strategy of ``CSVEncoderConfiguration/DateEncodingStrategy-swift.enum/iso8601``.
+    public private(set) var configuration: CSVEncoderConfiguration
+    
+    /// Create a CSV table definition.
+    /// - Parameters:
+    ///   - columns: a list of ``CSVColumn`` records describing each column of the CSV file output
+    ///   - configuration: the configuration to use when building the CSV file
     public init(
         columns: [CSVColumn<Record>],
         configuration: CSVEncoderConfiguration = .default
@@ -18,8 +74,13 @@ public struct CSVTable<Record> {
         self.columns = columns
         self.configuration = configuration
     }
-
-    public func export(rows: any Sequence<Record>) -> String {
+    
+    /// Constructs a CSV text file structure from the given rows of data.
+    /// - Parameter rows: a collection of objects which will be output, in order, as rows of the CSV file
+    /// - Returns: A String representing all supplied rows as a CSV file, including a header row
+    public func export(
+        rows: any Sequence<Record>
+    ) -> String {
         ([headers] + allRows(rows: rows)).newlineDelimited
     }
 
