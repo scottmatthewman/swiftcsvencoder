0fb585bf6609091344f8d2d3fd50e77f5a4820b6
Scott Matthewman <scott@matthewman.net>
2023-12-30 16:42:25 +0000

    First pass at building docs for GitHub Pages

--- Package.swift
+++ Package.swift
@@ -15,6 +15,9 @@ let package = Package(
             name: "SwiftCSVEncoder",
             targets: ["SwiftCSVEncoder"]),
     ],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-docc-plugin", exact: "1.1.0")
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
