259ee240af5c56d98645f785f091def1baf49ba4
Scott Matthewman <scott@matthewman.net>
2024-02-17 10:42:15 +0000

    Bringing repo up to date. I hate commits like this.

--- Package.swift
+++ Package.swift
@@ -19,10 +19,7 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "SwiftCSVEncoder",
-            resources: [
-                .copy("PrivacyInfo.xcprivacy")
-            ]
+            name: "SwiftCSVEncoder"
         ),
         .testTarget(
             name: "SwiftCSVEncoderTests",
